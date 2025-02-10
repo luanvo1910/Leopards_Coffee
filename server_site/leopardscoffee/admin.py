651dc1b32a5247bdaefdb9ba0576a115456149d3
luanvo1910 <vothanhluan19102001@gmail.com>
2025-02-10 20:40:15 +0700

    LEOCF-12 User model

--- server_site/leopardscoffee/admin.py
+++ server_site/leopardscoffee/admin.py
@@ -6,14 +6,30 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'isValid', 'category')
+    list_display = ('name', 'price', 'isValid', 'get_category_name')
     list_filter = ['category', 'isValid']
     search_fields = ['name']
+
+    def get_category_name(self, obj):
+        return obj.category.name
+    get_category_name.admin_order_field = 'category'
+    get_category_name.short_description = 'category'
 
 class RoleAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'get_role_name')
+    list_filter = ['role']
+    search_fields = ['name', 'email', 'phone']
+
+    def get_role_name(self, obj):
+        return obj.role.name
+    get_role_name.admin_order_field = 'role'
+    get_role_name.short_description = 'Role'
+
 admin.site.register(Categories, CategoryAdmin)
 admin.site.register(Products, ProductAdmin)
 admin.site.register(Roles, RoleAdmin)
+admin.site.register(Users, UserAdmin)
--- server_site/leopardscoffee/models.py
+++ server_site/leopardscoffee/models.py
@@ -4,14 +4,24 @@ from .custom_field import *
 
 class Categories(models.Model):
     name = models.CharField(unique=True, max_length=100)
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     name = models.CharField(unique=True, max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=3)
     isValid = CustomBooleanField()
-    image = models.ImageField(upload_to=get_file_path)
+    image = models.ImageField(upload_to=get_product_image_path)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE)
 
 class Roles(models.Model):
     name = models.CharField(unique=True, max_length=100)
+    def __str__(self):
+        return self.name
 
+class Users(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True, max_length=255)
+    phone = models.CharField(validators=[phone_regex], max_length=15, unique=True)
+    image = models.ImageField(upload_to=get_user_image_path)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE, default=2)
