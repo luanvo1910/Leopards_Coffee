7323a41f3a286edabc0d02fc2c04fd5a92bef6f6
luanvo1910 <vothanhluan19102001@gmail.com>
2025-02-17 08:38:55 +0700

    create menu page

--- client-site/src/App.js
+++ client-site/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/auth";
+import Home from "./pages/Home/index"
+import Menu from "./pages/Menu/menu"
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </Router>
   );
